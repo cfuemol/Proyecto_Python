254cdd1b4408141e20ce9594124513ebe8da0b45
José Ignacio <joseyenred@hotmail.com>
2025-05-20 12:54:30 +0200

    ruta login app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from passlib.hash import pbkdf2_sha256
 
 app = Flask(__name__)
 
+@app.route('/')
+def login():
+    return render_template('login.html')
 
 
-if __name__ == '__main__':
+if __name__== '__main__':
     app.run()
+
