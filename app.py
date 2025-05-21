469ccbfe2f301a77fc7abdd8ce2ab11b5b3e3ab3
José Ignacio <joseyenred@hotmail.com>
2025-05-21 10:14:26 +0200

    creando rutas para el dashboard de los usuarios

--- app.py
+++ app.py
@@ -7,8 +7,17 @@ app = Flask(__name__)
 def login():
     return render_template('login.html')
 
-@app.route('/dashboard/cliente')
+@app.route('/dashboard_cliente')
+def dashboard_cliente():
+    return render_template('dashboard_cliente.html')
 
+@app.route('/dashboard_admin')
+def dashboard_admin():
+    return render_template('dashboard_admin.html')
+
+@app.route('/dashboard_empleado')
+def dashboard_empleado():
+    return render_template('dashboard_empleado.html')
 if __name__== '__main__':
     app.run()
 
